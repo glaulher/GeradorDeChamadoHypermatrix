ae2c54647fc82d140519f815ed7caed7007737a4
glaulher <glaulher.developer@gmail.com>
2025-05-15 17:10:01 -0300

    feat: add  line break

--- src/utils/payload_utils.py
+++ src/utils/payload_utils.py
@@ -5,18 +5,23 @@ import pyperclip
 
 
 def payload_and_output(payload_dict: dict, output_file: str = "payload.json") -> str:
+    caller_filename = inspect.stack()[1].filename
 
-    # Sanitiza os dados
-    for k in payload_dict:
-        payload_dict[k] = (
-            str(payload_dict[k])
-            .replace("\r", "")
-            .replace("\n", "")
-            .replace("\t", "")
-            .strip()
-        )
+    if "control_pim_window.py" in caller_filename:
+        # Sanitiza os dados
+        for k in payload_dict:
+            payload_dict[k] = (
+                str(payload_dict[k])
+                .replace("\r", "")
+                .replace("\n", "")
+                .replace("\t", "")
+                .strip()
+            )
+    else:
+        for k in payload_dict:
+            payload_dict[k] = str(payload_dict[k]).replace("\t", "").strip()
 
-        # Salva o JSON
+    # Salva o JSON
     with open(output_file, "w", encoding="utf-8") as f:
         json.dump(payload_dict, f, ensure_ascii=False, indent=4)
 
@@ -26,8 +31,6 @@ def payload_and_output(payload_dict: dict, output_file: str = "payload.json") ->
             output_str += f"*{payload_dict[key]}*\n\n"
         else:
             output_str += f"*{key}*: {payload_dict[key]}\n"
-
-    caller_filename = inspect.stack()[1].filename
 
     if "control_pim_window.py" in caller_filename:
         pyperclip.copy("\t".join(v for k, v in payload_dict.items() if k != "Assunto"))
